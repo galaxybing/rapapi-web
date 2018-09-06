c287076292430b5d4c76035bad970adca4387f94
galaxyw <wangyh@317hu.com>
2018-09-06 12:04:46 +0800

    更新 联接接口弹层内查询接口下拉框的显示属性为visible

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = ({ counter = {} }) => (
     {/* v{counter.version} */}
     <p>317护 前端架构[fe-master]</p>
     <span className='ml10 mr10 color-c'>|</span>
-    {counter.users} 人正在使用 RAP
+    {counter.users} 人正在使用
     <span className='ml10 mr10 color-c'>|</span>
     今日 Mock 服务被调用 {counter.mock} 次
   </div>
